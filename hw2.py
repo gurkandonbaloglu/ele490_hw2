e3c1ff0d2731dadb71f105b362590bf6675196e0
gurkandonbaloglu <gdonbaloglu@hacettepe.edu.tr>
2024-10-19 22:45:54 +0300

    Update hw2.py

--- hw2.py
+++ hw2.py
@@ -5,34 +5,39 @@ import cv2
 from skimage import exposure
 import os
 
-lisa_image = cv2.imread('lisa.tif', cv2.IMREAD_GRAYSCALE)
-cameraman_image = cv2.imread('cameraman.tif', cv2.IMREAD_GRAYSCALE)
+cameraman_image = Image.open('cameraman.tif').convert('L')
+lisa_image = Image.open('lisa.tif').convert('L')
+
+# Convert images to numpy arrays for further processing
+cameraman_image_np = np.array(cameraman_image)
+lisa_image_np = np.array(lisa_image)
+
 output_path = 'output_images'
 if not os.path.exists(output_path):
     os.makedirs(output_path)
-plt.imshow(lisa_image, cmap='gray')
+plt.imshow(lisa_image_np, cmap='gray', vmin=0, vmax=255)
 plt.title('Lisa Image')
 plt.axis('off')
 plt.savefig(os.path.join(output_path, 'lisa_image.png'))
 plt.close()
 
-plt.imshow(cameraman_image, cmap='gray')
+plt.imshow(cameraman_image_np, cmap='gray', vmin=0, vmax=255)
 plt.title('Cameraman Image')
 plt.axis('off')
 plt.savefig(os.path.join(output_path, 'cameraman_image.png'))
 plt.close()
 
-plt.hist(lisa_image.ravel(), bins=256, alpha=0.5, label='Lisa Original')
+plt.hist(lisa_image_np.ravel(), bins=256, alpha=0.5, label='Lisa Original')
 plt.title('Histogram of Lisa')
 plt.savefig(os.path.join(output_path, 'histogram_of_lisa.png'))
 plt.close()
 
-plt.hist(cameraman_image.ravel(), bins=256, alpha=0.5, label='Cameraman Original')
+plt.hist(cameraman_image_np.ravel(), bins=256, alpha=0.5, label='Cameraman Original')
 plt.title('Histogram of Cameraman')
 plt.savefig(os.path.join(output_path, 'histogram_of_cameraman.png'))
 plt.close()
 
-lisa_matched_image = exposure.match_histograms(lisa_image, cameraman_image)
+lisa_matched_image = exposure.match_histograms(lisa_image_np, cameraman_image_np)
 plt.imshow(lisa_matched_image, cmap='gray')
 plt.title('Lisa to Cameraman Matched Image')
 plt.axis('off')
@@ -44,7 +49,7 @@ plt.title('Lisa Matched Histogram')
 plt.savefig(os.path.join(output_path, 'lisa_matched.png'))
 plt.close()
 
-cameraman_matched_image = exposure.match_histograms(cameraman_image, lisa_image)
+cameraman_matched_image = exposure.match_histograms(cameraman_image_np, lisa_image_np)
 plt.imshow(cameraman_matched_image, cmap='gray')
 plt.title('Cameraman to Lisa Matched Image')
 plt.axis('off')
@@ -56,7 +61,7 @@ plt.title('Cameraman Matched Histogram')
 plt.savefig(os.path.join(output_path, 'cameraman_matched.png'))
 plt.close()
 
-lisa_equalized_image = cv2.equalizeHist(lisa_image)
+lisa_equalized_image = cv2.equalizeHist(lisa_image_np)
 plt.imshow(lisa_equalized_image, cmap='gray')
 plt.title('Lisa Equalized Image')
 plt.axis('off')
@@ -68,7 +73,7 @@ plt.title('Lisa Equalized Histogram')
 plt.savefig(os.path.join(output_path, 'lisa_equalized_histogram.png'))
 plt.close()
 
-cameraman_equalized_image = cv2.equalizeHist(cameraman_image)
+cameraman_equalized_image = cv2.equalizeHist(cameraman_image_np)
 plt.imshow(cameraman_equalized_image,cmap='gray')
 plt.title('Cameraman Equalized Image')
 plt.axis('off')
@@ -84,12 +89,12 @@ plt.close()
 plt.figure(figsize=(10, 10))
 
 plt.subplot(3, 2, 1)
-plt.imshow(lisa_image, cmap='gray')
+plt.imshow(lisa_image_np, cmap='gray', vmin=0, vmax=255)
 plt.title("Original Lisa Image")
 plt.axis('off')
 
 plt.subplot(3, 2, 2)
-plt.imshow(cameraman_image, cmap='gray')
+plt.imshow(cameraman_image_np, cmap='gray', vmin=0, vmax=255)
 plt.title("Original Cameraman Image")
 plt.axis('off')
 
@@ -106,16 +111,16 @@ plt.axis('off')
 
 # Showing Histograms
 plt.subplot(3, 2, 5)
-plt.hist(lisa_image.ravel(), bins=256, alpha=0.5, label='Lisa Original')
+plt.hist(lisa_image_np.ravel(), bins=256, alpha=0.5, label='Lisa Original')
 plt.hist(lisa_matched_image.ravel(), bins=256, alpha=0.5, label='Lisa Matched')
-plt.hist(cameraman_image.ravel(), bins=256, alpha=0.5, label='Cameraman Original')
+plt.hist(cameraman_image_np.ravel(), bins=256, alpha=0.5, label='Cameraman Original')
 plt.legend(loc='upper right')
 plt.title("Histogram Comparison")
 
 plt.subplot(3, 2, 6)
-plt.hist(cameraman_image.ravel(), bins=256, alpha=0.5, label='Cameraman Original')
+plt.hist(cameraman_image_np.ravel(), bins=256, alpha=0.5, label='Cameraman Original')
 plt.hist(cameraman_matched_image.ravel(), bins=256, alpha=0.5, label='Cameraman Matched')
-plt.hist(lisa_image.ravel(), bins=256, alpha=0.5, label='Lisa Original')
+plt.hist(lisa_image_np.ravel(), bins=256, alpha=0.5, label='Lisa Original')
 plt.legend(loc='upper right')
 plt.title("Histogram Comparison")
 
